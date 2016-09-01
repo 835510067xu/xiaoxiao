b1ef1d4337c975dd820d0bdbb639c8ed8edd43b0
835510067xu <student@MacdeiMac-98.local>
2016-09-01 20:26:33 +0800

    xyh
    我提交了

--- 调试/ViewController.m
+++ 调试/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //老婆婆婆婆婆婆婆啦啦啦啦啦啦啦啦啦啦啦啦啦啦
 }
 
 - (void)didReceiveMemoryWarning {
